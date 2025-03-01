5def5654f6b59a145a6978e1e25820056e1fbed1
Sergio <sagdpr03@gmail.com>
2025-02-28 22:55:22 -0500

    upload types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -8,13 +8,19 @@ export interface MusicFile {
 export interface CoverUpdate {
   update: boolean;
   bytes: Uint8Array;
+  mimeType: string;
+}
+
+export interface CoverReq {
+  data: string;
+  type: string;
 }
 
 export interface Metadata {
   title: string;
   artist: string;
   album: string;
-  year: string;
+  year: number;
   genre: string;
   // coverUrl: string;
 }
